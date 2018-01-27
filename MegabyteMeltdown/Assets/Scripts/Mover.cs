a35da4a1e6f70e15449f5b028c3d2441b9e8d844
AlainHubGit <alainmaubert@orland111.csdyn.uregina.ca>
2018-01-27 14:36:07 -0600

    Added Colliders
    Added Collider Script. Ground getting destroyed though.

---
+++ MegabyteMeltdown/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float speed;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = transform.forward * speed;
+	}
+}
--- MegabyteMeltdown/Assets/Scripts/playerController.cs
+++ MegabyteMeltdown/Assets/Scripts/playerController.cs
@@ -1,39 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class playerController : MonoBehaviour {
-    public float speed = 5.0f;
-    public float jumpPower = 5.0f;
-    public bool isGrounded = false;
-	// Use this for initialization
-	void Start () {
-      
-    }
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
 	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(new Vector2(-speed * Time.deltaTime, 0.0f));
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(new Vector2(speed * Time.deltaTime, 0.0f));
-        }
-        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpPower), ForceMode2D.Impulse);
-            isGrounded = false;
-        }
-    }
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5.0f;
+	public float jumpPower = 5.0f;
+	public bool isGrounded = false;
+	public Rigidbody rb;
+	public AudioSource audio;
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "ground")
-        {
-            isGrounded = true;
-        }
-    }
-    
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		audio = GetComponent<AudioSource> ();
+	}
+	// For player movement
+	public float speed2;
+	public float tilt;
+	public Boundary boundary;
+
+	// For Shooting
+	public GameObject projectile;
+	public float fireDelta = 0.5f;
+	private float nextFire = 0.5f;
+	private GameObject newProjectile;
+	private float myTime = 0.0f;
+	public Transform shotSpawn;
+
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector2(-speed * Time.deltaTime, 0.0f));
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector2(speed * Time.deltaTime, 0.0f));
+		}
+		if (Input.GetKey(KeyCode.Space) && isGrounded == true)
+		{
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpPower), ForceMode2D.Impulse);
+			isGrounded = false;
+		}
+
+		myTime = myTime + Time.deltaTime;
+
+		if(Input.GetButton("Fire1") && myTime > nextFire)
+		{
+			nextFire = myTime + fireDelta;
+			//newProjectile = 
+			Instantiate (projectile, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+			// audio
+			audio.Play ();
+
+			nextFire = nextFire - myTime;
+			myTime = 0.0f;
+		}
+
+	}
+
+
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "ground")
+		{
+			isGrounded = true;
+		}
+	}
+
 }
