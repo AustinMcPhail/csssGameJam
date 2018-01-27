e8b97a901d26c9391ae016df757bc5651f868351
Austin McPhail <austin.mcphail@hotmail.com>
2018-01-26 23:18:00 -0600

    Jumping now checks if player is grounded

--- MegabyteMeltdown/Assets/Scripts/playerController.cs
+++ MegabyteMeltdown/Assets/Scripts/playerController.cs
@@ -3,21 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
-    public float speed = 10;
-    public float jumpPower = 5;
-    public bool isGrounded = true;
+    public float speed = 5.0f;
+    public float jumpPower = 5.0f;
+    public bool isGrounded = false;
 	// Use this for initialization
 	void Start () {
-		
-	}
+      
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Move();
-	}
-
-    public void Move()
-    {
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(new Vector2(-speed * Time.deltaTime, 0.0f));
@@ -26,9 +21,20 @@ public class playerController : MonoBehaviour {
         {
             transform.Translate(new Vector2(speed * Time.deltaTime, 0.0f));
         }
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded == true)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 1.0f), ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, jumpPower), ForceMode2D.Impulse);
+            isGrounded = false;
         }
     }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+
+        }
+    }
+    
 }
