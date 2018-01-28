2348ff701fa1d3d65e4683a47d02324183c34cc3
birdmanscrest <anthonylandgraf@live.ca>
2018-01-28 00:42:11 -0600

    Pause is working 80 percent.

--- MegabyteMeltdown/Assets/Scripts/PauseController.cs
+++ MegabyteMeltdown/Assets/Scripts/PauseController.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    SceneManager scene = new SceneManager();
+
+    public static PauseController instance;
+
+    [SerializeField]
+    private Text scoreText, coinText, lifeText;
+
+    [SerializeField]
+    private GameObject pausePanel;
+
+    // Use this for initialization
+    void Awake () {
+        MakeInstance(); 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void MakeInstance () {
+		if(instance == null)
+        {
+            instance = this;
+        }
 	}
+
+    public void PauseTheGame()
+    {
+        Time.timeScale = 0f;
+        //GameObject.Find("Pause Panel").SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        //GameObject.Find("Pause Panel").SetActive(false);
+        //pausePanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        //Application.LoadLevel("MainMenu");
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
