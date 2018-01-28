696539905848a7b36c20931376315b4cdb65dc45
Austin McPhail <austin.mcphail@hotmail.com>
2018-01-27 18:20:05 -0600

    created fileController script

---
+++ MegabyteMeltdown/Assets/Scripts/fileController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fileController : MonoBehaviour {
+
+    float maxUpAndDown = 0.005f;
+    float speed = 100.0f;
+    float angle = 0.0f;
+    float toDegrees = Mathf.PI / 180;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        angle += speed * Time.deltaTime;
+        if (angle > 360) angle -= 360;
+        transform.Translate(new Vector3(0.0f, maxUpAndDown * Mathf.Sin(angle * toDegrees), 0.0f));
+    }
+}
+
